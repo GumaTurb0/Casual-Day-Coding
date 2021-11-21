e7342f83085cc8ec16b4fb3b587e7b880ebc72e4
GumaTurb0 <90203301+GumaTurb0@users.noreply.github.com>
2021-11-21 22:18:06 +0100

    Pierwszy program 3

---
+++ src/PierwszyProgram3.java
@@ -0,0 +1,7 @@
+public class PierwszyProgram3 {
+    public static void main(String[] args) {
+        System.out.println("Dzisiaj zaczynam kurs");
+        System.out.println("JAVA");
+        System.out.println("w CodersLab");
+    }
+}
