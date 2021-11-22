0c8b467fd01524cbff5e1fcb71230ffb24da6259
GumaTurb0 <90203301+GumaTurb0@users.noreply.github.com>
2021-11-22 22:38:45 +0100

    Nowa klasa, zadania z książki

--- src/PierwszyProgram4.java
+++ src/PierwszyProgram4.java
@@ -1,3 +1,13 @@
 public class PierwszyProgram4 {
-
-}
+    public static void main(String[] args) {
+        int x = 1;
+        while (x<2){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+        if (x == 3){
+            System.out.print("Do");
+        }
+    }
+    }
---
+++ src/classCwiczenie1A.java
@@ -0,0 +1,41 @@
+public class classCwiczenie1A {
+    public static void main(String[] args) {
+        int z = 1;
+        while (z < 10) {
+            z = z + 1;
+            if (z > 3) {
+                System.out.println("wielkie X");
+            }
+        }
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("Malutkie x" +
+                        "");
+            }
+        }
+
+
+            int a = 0;
+            while (a < 4) {
+                System.out.print("a");
+                if (a < 1) {
+                    System.out.print(" ");
+                }
+                System.out.print("n");
+                if (a > 1) {
+                    System.out.print("tyczny");
+                    a = a + 2;
+                }
+                if (a == 1) {
+                    System.out.print("talek");
+                }
+                if (a < 1) {
+                    System.out.print("asz");
+                }
+                System.out.println("");
+                a = a + 1;
+            }
+        }
+}
