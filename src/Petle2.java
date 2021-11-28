65255731bced82cd55e970763337eaec93a42b2d
GumaTurb0 <90203301+GumaTurb0@users.noreply.github.com>
2021-11-28 16:10:25 +0100

    Zadania z preworka Pętle

---
+++ src/Petle2.java
@@ -0,0 +1,21 @@
+public class Petle2 {
+    public static void main(String[] args) {
+        int nr1 = 3;
+        int nr2 = 7;
+        int nr3 = 12;
+        if ((nr1 > nr2) && (nr1 > nr3)) {
+            System.out.println("Największa liczba to " + nr1);
+        } else if
+        ((nr2 > nr3) && (nr2 > nr1)) {
+            System.out.println("Największa liczba to " + nr2);
+        } else {
+            System.out.println("Największa liczba to " + nr3);
+        }
+    }
+}
+
+
+
+//    W pliku Main02.java stwórz trzy zmienne, o nazwach nr1, nr2, nr3, przechowujące liczby całkowite o wartościach 3, 7, 11.
+//        Następnie za pomocą instrukcji warunkowej if ... else if ... else, wypisz na stronie, która z nich jest największa.
+//        Tekst wypisz wg wzoru: Największa z liczb to 11.
